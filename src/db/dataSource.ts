607632f75b7211f400346dd8f49616b2e302ce19
NiklasFaust <Niklas.Faust@freenet.ag>
2026-02-19 08:57:12 +0100

    fix: update database connection settings to use environment variables

--- src/db/dataSource.ts
+++ src/db/dataSource.ts
@@ -10,29 +10,13 @@ import { KitProduct } from "../entities/kitProduct";
 import { Entry } from "../entities/entry";
 import { EntryItem } from "../entities/entryItem";
 
-// export const AppDataSource = new DataSource({
-//   type: "postgres",
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT ?? 5433),
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//
-//   entities: [User, FirstAidKit, Product, KitProduct, Entry, EntryItem],
-//
-//   migrations: ["src/db/migrations/*.{ts,js}"],
-//
-//   synchronize: true,
-//   logging: false
-// });
-
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: Number(process.env.DB_PORT ?? 5433),
-  username: "dvb",
-  password: "dvb",
-  database: "dvb",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 
   entities: [User, FirstAidKit, Product, KitProduct, Entry, EntryItem],
 
@@ -41,3 +25,4 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: false
 });
+
